c4ba716d9a0681ca02e1ea05973efced8cd0779d
Laolu <Laolu@qq.com>
2022-04-22 21:02:44 +0800

    first commit

---
+++ src/main/java/com/laolu/test/GitTest.java
@@ -0,0 +1,12 @@
+package com.laolu.test;
+
+/**
+ * @author LaoLu
+ * @Description:
+ * @create 2022-04-22 20:58
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git!!");
+    }
+}
